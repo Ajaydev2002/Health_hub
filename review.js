6786683f6ec8b67e2f9015dcc5c92f1795755575
ajaydev2002 <ajay.p@rigelsoft.in>
2024-12-18 19:03:25 +0530

    Normalize line endings

--- review.js
+++ review.js
@@ -1,2 +1,14 @@
-const profileImages = document.querySelectorAll('.profile-images img');
-const reviewContent = document.querySelectorAll('.review-content');
+const profiles = document.querySelectorAll('.profile-images img');
+const Contents = document.querySelectorAll('.review-content');
+
+profiles.forEach(profile => {
+
+    profile.addEventListener('click', () => {
+
+        Contents.forEach(content => content.classList.remove('active'));
+
+        const targetId = profile.getAttribute('data-target');
+
+        document.getElementById(targetId).classList.add('active');
+    });
+});
